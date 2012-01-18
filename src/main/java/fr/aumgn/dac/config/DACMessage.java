e2183366dc51de5da0395db5e5caaa41ba7585b5
aumgn <aumgn@free.fr>
2012-01-18 13:22:06 +0100

    DACMessage.java format

--- src/main/java/fr/aumgn/dac/config/DACMessage.java
+++ src/main/java/fr/aumgn/dac/config/DACMessage.java
@@ -9,13 +9,13 @@ import org.bukkit.configuration.Configuration;
 import fr.aumgn.dac.DAC;
 
 public enum DACMessage {
-	
+
 	CmdDefineExists			("command.define.exists"),
 	CmdDefineSuccess		("command.define.success"),
 	CmdDeleteUnknown		("command.delete.unknown"),
 	CmdDeleteInGame			("command.delete.in-game"),
 	CmdDeleteSuccess		("command.delete.success"),
-    CmdGotoNotInGame		("command.goto.not-in-game"),
+	CmdGotoNotInGame		("command.goto.not-in-game"),
 	CmdGotoSuccess			("command.goto.success"),
 	CmdJoinAlreadyPlaying	("command.join.already-playing"),
 	CmdJoinNotInStart		("command.join.not-in-start"),
@@ -28,35 +28,35 @@ public enum DACMessage {
 	CmdResetUnknown			("command.reset.unknown"),
 	CmdResetInGame			("command.reset.in-game"),
 	CmdResetSuccess			("command.reset.success"),
-    CmdSelectUnknown		("command.select.unknown"),
-    CmdSelectSuccessPool	("command.select.success-pool"),
-    CmdSelectSuccessStart	("command.select.success-start"),
-    CmdSelectError			("command.select.error"),
-    CmdSetUnknown			("command.set.unknown"),
-    CmdSetWrongWorld		("command.set.another-world"),
-    CmdSetSuccessDiving		("command.set.success-diving"),
-    CmdSetSuccessPool		("command.set.success-pool"),
-    CmdSetSuccessStart		("command.set.success-start"),
-    CmdSetError				("command.set.error"),
-    CmdSetIncompleteRegion	("command.set.incomplete-selection"),
-    CmdStartNotInGame		("command.start.not-in-game"),
-    CmdStartMinNotReached	("command.start.min-not-reached"),
-    CmdStopNoGameToStop		("command.stop.no-game-to-stop"),
+	CmdSelectUnknown		("command.select.unknown"),
+	CmdSelectSuccessPool	("command.select.success-pool"),
+	CmdSelectSuccessStart	("command.select.success-start"),
+	CmdSelectError			("command.select.error"),
+	CmdSetUnknown			("command.set.unknown"),
+	CmdSetWrongWorld		("command.set.another-world"),
+	CmdSetSuccessDiving		("command.set.success-diving"),
+	CmdSetSuccessPool		("command.set.success-pool"),
+	CmdSetSuccessStart		("command.set.success-start"),
+	CmdSetError				("command.set.error"),
+	CmdSetIncompleteRegion	("command.set.incomplete-selection"),
+	CmdStartNotInGame		("command.start.not-in-game"),
+	CmdStartMinNotReached	("command.start.min-not-reached"),
+	CmdStopNoGameToStop		("command.stop.no-game-to-stop"),
 
-    JoinNewGame				("join.new-game"),
-    JoinNewGame2			("join.new-game2"),
-    JoinCurrentPlayers		("join.current-players"),
-    JoinPlayerList			("join.player-list"),
-    JoinPlayerJoin			("join.player-join"),
-    JoinPlayerQuit			("join.player-quit"),
-    JoinStopped				("join.stopped"),
-	
-    GameStart				("game.start"),
-    GamePlayers				("game.players"),
-    GamePlayerList			("game.player-list"),
-    GameEnjoy				("game.enjoy"),
+	JoinNewGame				("join.new-game"),
+	JoinNewGame2			("join.new-game2"),
+	JoinCurrentPlayers		("join.current-players"),
+	JoinPlayerList			("join.player-list"),
+	JoinPlayerJoin			("join.player-join"),
+	JoinPlayerQuit			("join.player-quit"),
+	JoinStopped				("join.stopped"),
+
+	GameStart				("game.start"),
+	GamePlayers				("game.players"),
+	GamePlayerList			("game.player-list"),
+	GameEnjoy				("game.enjoy"),
 	GamePlayerTurn			("game.player-turn"),
-    GameConfirmation		("game.confirmation"),
+	GameConfirmation		("game.confirmation"),
 	GameDACConfirmation		("game.dac-confirmation"),
 	GameDACConfirmation2	("game.dac-confirmation2"),
 	GameDAC					("game.dac"),
@@ -72,9 +72,9 @@ public enum DACMessage {
 	GameRank				("game.rank"),
 	GameStopped				("game.stopped"),
 	GameDisplayLives		("game.display-lives");
-	
+
 	private static Pattern pattern = Pattern.compile("<([A-Za-z]+)>"); 
-	
+
 	public static void load(Configuration config, Configuration defaults) {
 		for (DACMessage lang : DACMessage.values()) {
 			String node = lang.getNode();
@@ -87,7 +87,7 @@ public enum DACMessage {
 			}
 		}
 	}
-	
+
 	public static String parse(String node, String message) {
 		StringBuffer parsed = new StringBuffer();
 		Matcher matcher = pattern.matcher(message);
@@ -104,10 +104,10 @@ public enum DACMessage {
 		matcher.appendTail(parsed);
 		return parsed.toString();
 	}
-    
+
 	private String node;
 	private String value;
-	
+
 	private DACMessage(String node) {
 		this.node = node;
 		this.value = "";
@@ -116,7 +116,7 @@ public enum DACMessage {
 	public String getNode() {
 		return node;
 	}
-	
+
 	public String getValue() {
 		return value;
 	}
@@ -132,5 +132,5 @@ public enum DACMessage {
 	public String format(Object... args) {
 		return String.format(getValue(), args);
 	}
-	
+
 }
