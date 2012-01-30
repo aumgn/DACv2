40aa1abc70b508d15b097893dafe7fe50c1b4830
aumgn <aumgn@free.fr>
2012-01-30 13:08:35 +0100

    Refactoring DACUtil

--- src/main/java/fr/aumgn/dac/DACUtil.java
+++ src/main/java/fr/aumgn/dac/DACUtil.java
@@ -26,7 +26,7 @@ public final class DACUtil {
 	
 	private DACUtil() {} 
 	
-	public static int getModValue(double i) {
+	private static int getBlockFaceModValue(double i) {
 		if (i < -1 || i > 1) {
 			throw new IllegalArgumentException("Value must be between -1 and 1");
 		}
@@ -36,14 +36,14 @@ public final class DACUtil {
 		else if (i >  MOD_2_TO_1)  { return  1; }
 		else if (i >  MOD_1_TO_2)  { return  2; }
 		else if (i >  MOD_0_TO_1)  { return  1; }
-		else                    { return  0; }
+		else                       { return  0; }
 	}
 	
 	public static BlockFace getHorizontalFaceFor(Vector2D vec) {
 		try {
 			Vector2D dir = vec.normalize();
-			int modX = getModValue(dir.getX());
-			int modZ = getModValue(dir.getZ());
+			int modX = getBlockFaceModValue(dir.getX());
+			int modZ = getBlockFaceModValue(dir.getZ());
 			for (BlockFace face : BlockFace.values()) {
 				if (face.getModY() == 0 && face.getModX() == modX && face.getModZ() == modZ) {
 					return face;
@@ -54,44 +54,41 @@ public final class DACUtil {
 			return BlockFace.SELF;
 		}
 	}
-
 	
+	private static int getRelativeModValue(double decimal) {
+		if (decimal < 0.3) { return -1; }
+		if (decimal > 0.7) { return  1; }
+		return 0;
+	}
+	
+	private static boolean isRelativeBlockSolid(Block from, int modX, int modZ) {
+		Block block = from.getRelative(modX, 0, modZ);
+		return !block.isEmpty() && !block.isLiquid();
+	}
+
 	public static BlockVector getDeathBlockVector(Location loc) {
 		Block block = loc.getBlock().getRelative(0, -1, 0);
 		if (!block.isEmpty() && !block.isLiquid()) {
 			return new BlockVector(block.getX(), block.getY() + 1, block.getZ());
 		}
 		
-		int modX, modZ;
 		double x = Math.abs(loc.getX());
 		double z = Math.abs(loc.getZ());
 		double xDec = x - Math.floor(x);
 		double zDec = z - Math.floor(z);
-		Block block2;
-		if (xDec < 0.3) { modX = -1; }
-		else if (xDec > 0.7) { modX = 1; }
-		else { modX = 0; }
-		if (zDec < 0.3) { modZ = -1; }
-		else if (zDec > 0.7) { modZ = 1; }
-		else { modZ = 0; }
-		if (modX != 0) {
-			block2 = block.getRelative(modX, 0, 0);
-			if (!block2.isEmpty() && !block2.isLiquid()) {
-				return new BlockVector(block2.getX(), block2.getY() + 1, block2.getZ());
-			}
+		int modX = getRelativeModValue(xDec);
+		int modZ = getRelativeModValue(zDec);
+
+		if (modX != 0 && isRelativeBlockSolid(block, modX, 0)) {
+			return new BlockVector(block.getX() + modX, block.getY() + 1, block.getZ());
 		}
-		if (modZ != 0) {
-			block2 = block.getRelative(0, 0, modZ);
-			if (!block2.isEmpty() && !block2.isLiquid()) {
-				return new BlockVector(block2.getX(), block2.getY() + 1, block2.getZ());
-			}
+		if (modZ != 0 && isRelativeBlockSolid(block, 0, modZ)) {
+			return new BlockVector(block.getX(), block.getY() + 1, block.getZ() + modZ);
 		}
-		if (modX != 0 && modZ != 0) {
-			block2 = block.getRelative(modX, 0, modZ);
-			if (!block2.isEmpty() && !block2.isLiquid()) {
-				return new BlockVector(block2.getX(), block2.getY() + 1, block2.getZ());
-			}
+		if (modX != 0 && modZ != 0 && isRelativeBlockSolid(block, modX, modZ)) {
+			return new BlockVector(block.getX() + modX, block.getY() + 1, block.getZ() + modZ);
 		}
+	
 		return null;
 	}
 
