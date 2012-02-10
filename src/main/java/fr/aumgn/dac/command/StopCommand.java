d29ac4427c455461567b4e9b023f52cea219d65e
aumgn <aumgn@free.fr>
2012-02-10 22:20:09 +0100

    Add a message for dac stop

--- src/main/java/fr/aumgn/dac/command/StopCommand.java
+++ src/main/java/fr/aumgn/dac/command/StopCommand.java
@@ -18,6 +18,7 @@ public class StopCommand extends PlayerCommandExecutor {
 		if (stage == null) {
 			error(DACMessage.CmdStopNoGameToStop);
 		}
+		stage.send(DACMessage.GameStopped);
 		stage.stop();
 	}
 
