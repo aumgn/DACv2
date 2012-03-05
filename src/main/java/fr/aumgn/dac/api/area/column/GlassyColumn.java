d39948f8fccad633c86e6ea6def0d99af9f425be
aumgn <aumgn@free.fr>
2012-03-05 10:42:49 +0100

    Clean up GlassyColumn

--- src/main/java/fr/aumgn/dac/api/area/column/GlassyColumn.java
+++ src/main/java/fr/aumgn/dac/api/area/column/GlassyColumn.java
@@ -3,23 +3,20 @@ package fr.aumgn.dac.api.area.column;
 import org.bukkit.Material;
 
 import fr.aumgn.dac.api.area.AreaColumn;
-import fr.aumgn.dac.api.area.ColumnPattern;
 import fr.aumgn.dac.api.config.DACColor;
 
 /**
  * Represents a column which has a glass block on top.
  */
-public class GlassyColumn implements ColumnPattern {
-    
-    private DACColor color; 
+public class GlassyColumn extends UniformColumn {
     
     public GlassyColumn(DACColor color) {
-        this.color = color; 
+        super(color); 
     }
 
     @Override
     public void set(AreaColumn column) {
-        column.set(color.getMaterial(), color.getData());
+        super.set(column);
         column.get(-1).setType(Material.GLASS);
     }
 
