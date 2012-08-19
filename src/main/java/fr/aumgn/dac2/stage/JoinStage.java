bd8f8486d8fe867219d3e964b717ac2aad3f89d3
aumgn <aumgn@free.fr>
2012-08-19 19:42:52 +0200

    Add JoinStage

--- src/main/java/fr/aumgn/dac2/stage/JoinStage.java
+++ src/main/java/fr/aumgn/dac2/stage/JoinStage.java
@@ -1,18 +1,31 @@
 package fr.aumgn.dac2.stage;
 
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerQuitEvent;
+import java.util.List;
+import java.util.Map;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+
+import fr.aumgn.bukkitutils.localization.PluginMessages;
+import fr.aumgn.bukkitutils.playerid.map.PlayersIdHashMap;
+import fr.aumgn.bukkitutils.playerid.map.PlayersIdMap;
+import fr.aumgn.bukkitutils.util.Util;
+import fr.aumgn.dac2.DAC;
 import fr.aumgn.dac2.arena.Arena;
+import fr.aumgn.dac2.config.Color;
 
 public class JoinStage implements Stage, Listener {
 
+    private final DAC dac;
     private final Arena arena;
+    private final Map<String, Color> colors;
+    private final PlayersIdMap<Color> players;
 
-    public JoinStage(Arena arena) {
+    public JoinStage(DAC dac, Arena arena) {
+        this.dac = dac;
         this.arena = arena;
+        this.colors = dac.getColors().toMap();
+        this.players = new PlayersIdHashMap<Color>();
     }
 
     @Override
@@ -22,6 +35,10 @@ public class JoinStage implements Stage, Listener {
 
     @Override
     public void start() {
+        PluginMessages messages = dac.getMessages();
+        Util.broadcast(messages.get("joinstage.start1", arena.getName()));
+                //, "dac.game.watch");
+        Util.broadcast(messages.get("joinstage.start2"));//, "dac.game.watch");
     }
 
     @Override
@@ -30,15 +47,34 @@ public class JoinStage implements Stage, Listener {
 
     @Override
     public Listener[] getListeners() {
-        return new Listener[] { this };
+        return new Listener[0];
     }
 
     @Override
     public boolean contains(Player player) {
-        return false;
+        return players.containsKey(player);
     }
 
-    @EventHandler
-    public void onQuit(PlayerQuitEvent event) {
+    public void addPlayer(Player player, List<String> colorsName) {
+        Color color = null;
+        for (String rawColorName : colorsName) {
+            String colorName = rawColorName.toLowerCase();
+            if (colors.containsKey(colorName)) {
+                color = colors.get(colorName);
+                break;
+            }
+        }
+
+        addPlayer(player, color);
+    }
+
+    public void addPlayer(Player player, Color color) {
+        if (color == null || !colors.containsValue(color)) {
+            // Get first color available
+            color = colors.values().iterator().next();
+        }
+
+        players.put(player, color);
+        colors.remove(color.getName());
     }
 }
