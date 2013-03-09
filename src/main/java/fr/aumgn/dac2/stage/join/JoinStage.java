5dcbcd352457e01a9036e516cf81f4a67f7d2e3b
aumgn <aumgn@free.fr>
2013-03-09 12:04:28 +0100

    Derp

--- src/main/java/fr/aumgn/dac2/stage/join/JoinStage.java
+++ src/main/java/fr/aumgn/dac2/stage/join/JoinStage.java
@@ -93,7 +93,6 @@ public class JoinStage implements Stage, Listener, GameStartData {
                 break;
             }
         }
-        System.out.println(finalColor.name);
 
         addPlayer(player, finalColor);
     }
